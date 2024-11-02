26d38c26da8c99525b0410182222b16e62d1d8b6
Eng-Tobias <engtobiasguya100@gmail.com>
2024-11-01 17:49:29 -0700

    Create a program that generates zombie processes

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - function that runs indefinitely
+ * Return: 0 on success
+ */
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+/**
+ * main - creates 5 zombie processes
+ * Return: Always 0
+ */
+int main(void)
+{
+    pid_t pid;
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        pid = fork();  // Create a child process
+        if (pid > 0)  // This is the parent process
+        {
+            printf("Zombie process created, PID: %d\n", pid);
+        }
+        else if (pid == 0)  // This is the child process
+        {
+            exit(0);  // Child exits immediately, creating a zombie
+        }
+        else  // Fork failed
+        {
+            perror("Fork failed");
+            exit(1);
+        }
+    }
+    
+    infinite_while();  // Keep the parent process running
+    return (0);
+}
